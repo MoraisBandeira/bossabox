3cfc275666c6a079bdc6555248f58b4576959713
MoraisBandeira <anderson.moraisbandeira@outlook.com>
2024-01-15 01:04:53 -0300

    entidade tool

---
+++ src/entities/toolEntity.ts
@@ -0,0 +1,11 @@
+class ToolEntity {
+    constructor({ id, title, link, description, tags }) {
+      this.id = id;
+      this.title = title;
+      this.link = link;
+      this.description = description;
+      this.tags = tags;
+    }
+  }
+  
+  export default ToolEntity;
