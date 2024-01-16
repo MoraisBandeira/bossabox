7205e642ef0347a490dd21fcabfc7a35c9a073a0
MoraisBandeira <anderson.moraisbandeira@outlook.com>
2024-01-15 23:25:54 -0300

    refactor app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
+import toolRoutes from './routes/toolRoutes';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('ola mundo')
-})
+app.use('/', toolRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
