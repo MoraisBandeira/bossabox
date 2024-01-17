983fad48f19c2953d9fb9989d0d5de070a45c81b
MoraisBandeira <anderson.moraisbandeira@outlook.com>
2024-01-17 00:07:40 -0300

    import getToolsByTags

--- src/controllers/getToolsController.ts
+++ src/controllers/getToolsController.ts
@@ -1,9 +1,17 @@
 import { Request,Response } from "express";
 import getTools from "../use-cases/getTools";
+import getToolsByTags from "../use-cases/getToolsByTags";
 
 const getToolController = async (req:Request,res:Response)=>{
-    const tools = await getTools();
-     return res.status(200).json(tools)   
+    if(!req.query.tags){
+        const tools = await getTools();
+     return res.status(200).json(tools) 
+    }
+        const { tags } = req.query
+      const tools = await getToolsByTags(String(tags))
+      if(tools){
+        return res.status(200).json(tools)
+      }
 } 
 
 export default getToolController;
