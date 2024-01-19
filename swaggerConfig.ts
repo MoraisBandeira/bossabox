d2190170e27477afd4eb7f5352d54cac8ce2c6a9
MoraisBandeira <anderson.moraisbandeira@outlook.com>
2024-01-19 00:20:18 -0300

    documentação

--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Bossabox',
+      title: 'VUTTR (Very Useful Tools to Remember)',
       version: '1.0.0',
     },
   },
